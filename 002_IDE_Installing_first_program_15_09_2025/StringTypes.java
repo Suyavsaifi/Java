dc54549d987352ab8648310a8415382dfba24998
Suyavsaifi <muhdshoaibsaifi@gmail.com>
2025-09-14 20:23:52 +0530

    added new content for data types and variables

---
+++ 002_IDE_Installing_first_program_15_09_2025/StringTypes.java
@@ -0,0 +1,15 @@
+public class StringTypes {
+    public static void main(String[] args){
+//        String greeting = "Hello World";
+//        System.out.println(greeting);
+//
+//        // Another way of defining the string
+//        String greeting2 = new String("Hello World, I am happy to learn Java.");
+//        System.out.println(greeting2);
+         // escape character
+        String new_sent = "Fiona says, \"I am going to Florida.\"";
+        System.out.println(new_sent);
+        String new_sent2 = "Fiona says, \"I am going to Florida.\"\nbut I will come by evening.";
+        System.out.println(new_sent2);
+    }
+}
